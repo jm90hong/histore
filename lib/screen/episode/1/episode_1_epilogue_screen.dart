674bec723761432f21a7c2949e3136e9dcec286e
홍정민(GitMaster) <jm90.hong@gmail.com>
2022-07-16 10:24:55 +0900

    2022 06 09 01

--- lib/screen/episode/1/episode_1_epilogue_screen.dart
+++ lib/screen/episode/1/episode_1_epilogue_screen.dart
@@ -11,6 +11,10 @@ class _Episode1EpilogueScreenState extends State<Episode1EpilogueScreen> {
 
 
   var pageList=[
+    buildProCard(imgPath: 'assets/image/episode1/p1.jpg', onTap: (){}),
+    buildProCard(imgPath: 'assets/image/episode1/p2.jpg', onTap: (){}),
+    buildProCard(imgPath: 'assets/image/episode1/p3.jpg', onTap: (){}),
+    buildProCard(imgPath: 'assets/image/episode1/p4.jpg', onTap: (){}),
 
   ];
 
@@ -18,18 +22,41 @@ class _Episode1EpilogueScreenState extends State<Episode1EpilogueScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: Colors.red,
-        child: IndexedStack(
-          index: _currentIndex,
-          children: [
-
-          ],
+    return WillPopScope(
+      onWillPop: () async{
+        return true;
+      },
+      child: Scaffold(
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.white,
+          child: IndexedStack(
+            index: _currentIndex,
+            children: pageList,
+          ),
         ),
       ),
     );
   }
+
+
+}
+
+Widget buildProCard({required String imgPath,required Function onTap}){
+  return GestureDetector(
+    onTap: (){
+      onTap();
+    },
+    child: Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(imgPath,),
+          )
+      ),
+    ),
+  );
 }
