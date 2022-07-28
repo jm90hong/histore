81fac6955270f9122869952e6df31c3a07638df1
jm90hong <jm90.hong@gmail.com>
2022-07-28 14:02:17 +0900

    22 07 28 02

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,6 @@ class MyApp extends StatelessWidget {
     ]);
 
     return MultiProvider(
-
       providers: [
         ChangeNotifierProvider(create: (context)=>UserModel()),
         ChangeNotifierProvider(create: (context)=>GameResultModel()),
